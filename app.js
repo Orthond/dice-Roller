ed38a018a0062eca1979607c39c922bf52c2c1d1
Parke Hanlen <parkehanlenbusiness@gmail.com>
2020-11-19 03:06:30 -0700

    added all dice to the roller, can now increment the dice type upwards but not down

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const diceType = {
+const dice = {
     d4 : 4,
     d6 : 6,
     d8 : 8,
@@ -6,7 +6,28 @@ const diceType = {
     d12: 12,
     d20: 20
 }
-function roller(max){
-    return Math.floor(Math.random() * (max) + 1)
+let dSelect = 0;
+// initialize the dice selector, we will use this to index both our dice objects keys and values
+function diceVal(x){
+    let diceType = (Object.values(dice)[x]);
+    return diceType
 }
 
+function diceKey(y){
+    let diceType = (Object.keys(dice)[y]);
+    return diceType
+}
+// diceVal and diceKey are object selector functions for dice
+function roller(max){
+    let tester = (Math.floor(Math.random() * (max) + 1));
+    let rollVal = document.querySelector("#rollVal").textContent = `${tester}`;
+}
+document.querySelector("#testBtn").onclick = function() {roller(diceVal(dSelect))};
+// selects our roll button and calls roller to roll a dice based on the current dice value
+document.querySelector("#newBtn").onclick = function() {
+    dSelect++
+    document.querySelector("#testBtn").textContent = `Roll ${diceKey(dSelect)}`;
+}
+// increments the key/value selector by 1 and updates the roll buttons text to accuratley show its current dice key 
+document.querySelector("#testBtn").textContent = `Roll ${diceKey(dSelect)}`;
+// failsafe for always displaying the current Dice via the dice key on startup
